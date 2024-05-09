d8b97c94a04fa2b119381b485364e615397b017a
Sidney Rodrigues <sidneyr_gyn@hotmail.com>
2024-05-09 19:29:47 -0300

    comentarios-js

--- main.js
+++ main.js
@@ -44,8 +44,29 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
     console.log(contador);
 
-   
-
 }
 
 
+// SOBRE CODIGO
+
+// Função "tocaSom(seletorAudio)":
+// Essa função recebe um "seletorAudio" como parâmetro.
+// Ela utiliza o d"ocument.querySelector()" para selecionar o elemento correspondente ao "seletorAudio".
+// Em seguida, verifica se o "elemento" existe e se seu "localName" é igual a 'audio'.
+// Caso a condição seja verdadeira, a função chama o método" play()" no "elemento".
+// Caso contrário, a função exibe a mensagem "Elemento não encontrado ou seletor inválido" no console.
+
+// Seleção das teclas:
+// O código seleciona todas as teclas com a classe '.tecla' usando "document.querySelectorAll()".
+// Ele então itera sobre essa lista de teclas usando um "for".
+// Manipulação de eventos:
+
+// Para cada tecla, o código adiciona um evento de clique "(onclick)" que chama a função "tocaSom()" passando o "idAudio" correspondente.
+// Também são adicionados eventos de teclado" (onkeydown e onkeyup)" para simular a ativação e desativação da tecla.
+// No evento "onkeydown", o código verifica se a tecla pressionada é "Enter" ou "Space" e, se for, adiciona a classe 'ativa' à tecla.
+// No evento "onkeyup", o código remove a classe 'ativa' da tecla.
+
+// Exibição do contador:
+// Por fim, o código exibe o valor do "contador" no console a cada iteração do loop.
+
+// Esse código implementa a funcionalidade de tocar sons ao clicar ou pressionar as teclas de um instrumento musical virtual. A função "tocaSom()" é responsável por verificar se o elemento de áudio correspondente existe e, caso exista, reproduzir o som. O restante do código trata da manipulação dos eventos de clique e teclado nas teclas do instrumento.
